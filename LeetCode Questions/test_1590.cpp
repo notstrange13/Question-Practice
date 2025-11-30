29be09a2ccfed76a3fd58942ad4ec157adce312d
notstrange13 <mailtoshibasis13@gmail.com>
2025-12-01 02:31:03 +0530

    new changes and optimized

--- LeetCode Questions/test_1590.cpp
+++ LeetCode Questions/test_1590.cpp
@@ -5,42 +5,37 @@ class Solution
 public:
     int minSubarray(vector<int> &nums, int p)
     {
-        long long sum1, sum2 = 0;
-        int diff = 0;
+        long sum1;
+        long sum2 = 0;
         int ans = nums.size();
         int rem = 0;
         int len = 0;
         int prefix = 0;
-        unordered_map<int, int> hmap = {{0, -1}};
-        if (nums.size() == 0)
-        {
-            return 0;
-        }
+        unordered_map<int, int> hmap;
+        hmap[0] = -1;
         for (int i = 0; i < nums.size(); i++)
         {
             sum1 = sum1 + nums[i];
         }
         rem = sum1 % p;
-        if (rem == p)
+        if (rem == 0)
         {
             return 0;
         }
-        for (int i = 0; i < nums.size(); i++)
+        for (int i = 0; i < nums.size(); ++i)
         {
             sum2 = (sum2 + nums[i]) % p;
             prefix = (sum2 - rem + p) % p;
-            auto bol = hmap.find(prefix);
-            if (bol != hmap.end())
+            if (hmap.find(prefix) != hmap.end())
             {
                 len = i - hmap[prefix];
-                cout << len << endl;
+               
                 ans = min(ans, len);
-                cout << "hello" << endl;
+                
             }
-            hmap[prefix] = i;
+            hmap[sum2] = i;
         }
-        if (nums.size() == len)
-            return -1;
+        if(nums.size()==len) return -1;
         return ans;
     }
 };
